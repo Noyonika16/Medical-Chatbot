92df7eef31c9c5b91cdbf7f4185b929dd7a583df
Noyonika Mukherjee <noyonikacarmel@gmail.com>
2026-06-12 13:31:57 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Noyonika Mukherjee",
+    author_email="noyonikacarmel@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
